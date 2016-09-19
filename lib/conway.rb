f259ebe55d0520ff97ab3d0f7232d4525ea641e6
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-19 15:01:03 -0400

    Work on initialize World method
    Work on initialize World method

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,9 +1,18 @@
 class World
   # reader/writer methods?
+  attr_reader :width, :length, :cells
   def initialize(width, length)
     @cells = []
     @width = width
     @length = length
+  end
+
+  def populate_board
+    width.times do |row|
+      length.times do |column|
+        
+      end
+    end
   end
 end
 
@@ -11,4 +20,6 @@ class Cell
   def initialize
     @alive = false
   end
+
+
 end
